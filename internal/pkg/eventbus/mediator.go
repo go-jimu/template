0a18e687c2c0f24adb81bfae408af2ccb116be53
jacexh <jace.xh@gmail.com>
2025-09-24 03:28:32 +0000

    update

--- internal/pkg/eventbus/mediator.go
+++ internal/pkg/eventbus/mediator.go
@@ -12,9 +12,6 @@ func NewMediator(opt mediator.Options, logger *slog.Logger) mediator.Mediator {
 	logger = logger.With("name", "eventbus")
 
 	m := mediator.NewInMemMediator(opt).(*mediator.InMemMediator)
-	m.WithOrphanEventHandler(func(event mediator.Event) {
-		logger.Warn("find orphan event", slog.String("event_kind", string(event.Kind())))
-	})
 	m.WithGenContext(func(ctx context.Context, event mediator.Event) context.Context {
 		eventLogger := logger.With(slog.String("event_kind", string(event.Kind())))
 		ctx = sloghelper.NewContext(ctx, eventLogger)
